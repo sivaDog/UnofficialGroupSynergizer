a5a569969459f1cdedcbe86c55977d5e4c8a2f06
sivaDog <shibadog4525@gmail.com>
2025-07-09 22:10:51 +0900

    Reduce code in ActivityFinder

--- GroupSynergizerActivityFinder.lua
+++ GroupSynergizerActivityFinder.lua
@@ -1,45 +1,33 @@
-local bNotifyActive	= false;
+local bNotifyActive = false
 
-function GROUP_SYNERGIZER.OnActivityFinderStatusUpdate(eventCode, statusCode)
-
+function GROUP_SYNERGIZER.OnActivityFinderStatusUpdate(_, statusCode)
 	if not GROUP_SYNERGIZER.Enable then return end
-	
-	 -- stop multiple checks of the same statusCode in succession
+
+	-- stop multiple checks of the same statusCode in succession
 	if GROUP_SYNERGIZER.activityFinderCode == statusCode then
 		if statusCode == ACTIVITY_FINDER_STATUS_READY_CHECK and not HasLFGReadyCheckNotification() then bNotifyActive = false end -- already had ready check and confirmed, disable loop(s)
 		return
 	end
 	GROUP_SYNERGIZER.activityFinderCode = statusCode
-	--GROUP_SYNERGIZER.printLFGDebug(GROUP_SYNERGIZER.activityFinderCode)
 
 	local function LoopSoundNotify()
-		if not GROUP_SYNERGIZER.Enable
-		or not HasLFGReadyCheckNotification()
-		or not bNotifyActive
-		then return end
-
+		if not GROUP_SYNERGIZER.Enable or not HasLFGReadyCheckNotification() or not bNotifyActive then return end
 		PlaySound(SOUNDS.QUEST_SHARE_SENT)
 		PlaySound(SOUNDS.QUEST_SHARE_SENT)
 		zo_callLater(function() PlaySound(SOUNDS.QUEST_STEP_FAILED) end, 333)
-		zo_callLater(function() LoopSoundNotify() end, GROUP_SYNERGIZER.NotifyDelay * 1000)
+		zo_callLater(LoopSoundNotify, GROUP_SYNERGIZER.NotifyDelay * 1000)
 	end
 
 	local function LoopScreenNotify(messageText)
-		if not GROUP_SYNERGIZER.Enable
-		or not HasLFGReadyCheckNotification()
-		or not bNotifyActive
-		then return end
-		
+		if not GROUP_SYNERGIZER.Enable or not HasLFGReadyCheckNotification() or not bNotifyActive then return end
 		GROUP_SYNERGIZER.printCenter(messageText)
 		zo_callLater(function() LoopScreenNotify(messageText) end, 5000)
 	end
 
 	if GROUP_SYNERGIZER.EnhanceGAF then
-
 		-- checkbox
 		if GROUP_SYNERGIZER_AutoAccept ~= nil then
 			GROUP_SYNERGIZER.checkAuto.top = 0
-
 			if GROUP_SYNERGIZER.perfectPixelCompat then
 				if	statusCode == ACTIVITY_FINDER_STATUS_QUEUED or
 					statusCode == ACTIVITY_FINDER_STATUS_IN_PROGRESS or
@@ -51,84 +39,72 @@ function GROUP_SYNERGIZER.OnActivityFinderStatusUpdate(eventCode, statusCode)
 					GROUP_SYNERGIZER.checkAuto.top = -64
 				end
 			end
-			--GROUP_SYNERGIZER.AutoAcceptCheckbox(GROUP_SYNERGIZER.checkAuto.visible)
 			GROUP_SYNERGIZER.AutoAcceptCheckbox(true)
 		end
 
 		-- button
-		if GROUP_SYNERGIZER.checkPledges.button ~=nil then
+		local btn = GROUP_SYNERGIZER.checkPledges and GROUP_SYNERGIZER.checkPledges.button
+		if btn ~= nil then
 			if	statusCode == ACTIVITY_FINDER_STATUS_QUEUED or
 				statusCode == ACTIVITY_FINDER_STATUS_IN_PROGRESS or
 				statusCode == ACTIVITY_FINDER_STATUS_FORMING_GROUP or
 				statusCode == ACTIVITY_FINDER_STATUS_READY_CHECK
 			then
 				if GROUP_SYNERGIZER.perfectPixelCompat then
-					GROUP_SYNERGIZER.checkPledges.button:SetHidden(true)
+					btn:SetHidden(true)
 				else
-					GROUP_SYNERGIZER.checkPledges.button:SetState(BSTATE_DISABLED)
+					btn:SetState(BSTATE_DISABLED)
 					GROUP_SYNERGIZER.checkPledges.state = BSTATE_DISABLED
 				end
-			else			
-				GROUP_SYNERGIZER.checkPledges.button:SetState(BSTATE_NORMAL)
+			else
+				btn:SetState(BSTATE_NORMAL)
 				GROUP_SYNERGIZER.checkPledges.state = BSTATE_NORMAL
-				--if GROUP_SYNERGIZER.perfectPixelCompat then GROUP_SYNERGIZER.checkPledges.button:SetHidden(false) end
 			end
 		end
-
 	end
 
 	if GROUP_SYNERGIZER.perfectPixelCompat and GROUP_SYNERGIZER_PledgesCheck ~= nil then
-		if not GROUP_SYNERGIZER.showSpecificDung or statusCode == ACTIVITY_FINDER_STATUS_QUEUED then
-			GROUP_SYNERGIZER.checkPledges.button:SetHidden(true)
-		else
-			GROUP_SYNERGIZER.checkPledges.button:SetHidden(false)
+		local btn = GROUP_SYNERGIZER.checkPledges and GROUP_SYNERGIZER.checkPledges.button
+		if btn then
+			if not GROUP_SYNERGIZER.showSpecificDung or statusCode == ACTIVITY_FINDER_STATUS_QUEUED then
+				btn:SetHidden(true)
+			else
+				btn:SetHidden(false)
+			end
 		end
 	end
 
 	-- lfg not qued, qued, in progress, forming, or complete
-	if statusCode ~= ACTIVITY_FINDER_STATUS_READY_CHECK	or not HasLFGReadyCheckNotification() then
+	if statusCode ~= ACTIVITY_FINDER_STATUS_READY_CHECK or not HasLFGReadyCheckNotification() then
 		bNotifyActive = false
 		return
 	end
 
 	-- lfg ready check
-	if statusCode == ACTIVITY_FINDER_STATUS_READY_CHECK and not bNotifyActive then
-
+	if not bNotifyActive then
 		bNotifyActive = true
-
 		if GROUP_SYNERGIZER.AutoAccept then
-
 			if GROUP_SYNERGIZER.ScreenNotify then
 				GROUP_SYNERGIZER.print(GetString(SI_GROUP_SYNERGIZER_LFG_CHAT_A))
 				GROUP_SYNERGIZER.printCenter(GetString(SI_GROUP_SYNERGIZER_LFG_SCREEN))
 			end
-
 			if GROUP_SYNERGIZER.SoundNotify then
 				PlaySound(SOUNDS.QUEST_SHARE_SENT)
 				PlaySound(SOUNDS.QUEST_SHARE_SENT)
 			end
-
 			-- brief delay to allow lfg check to show before accept
-			zo_callLater(function() AcceptLFGReadyCheckNotification() end, 3000)
-
+			zo_callLater(AcceptLFGReadyCheckNotification, 3000)
 		else
-			
 			if GROUP_SYNERGIZER.ScreenNotify then
 				GROUP_SYNERGIZER.print(GetString(SI_GROUP_SYNERGIZER_LFG_CHAT))
 				LoopScreenNotify(GetString(SI_GROUP_SYNERGIZER_LFG_SCREEN))
 			end
-
-			if GROUP_SYNERGIZER.SoundNotify then LoopSoundNotify() end			
-
+			if GROUP_SYNERGIZER.SoundNotify then LoopSoundNotify() end
 		end
-
 	end
-	
 end
 
-function GROUP_SYNERGIZER.OnCooldownsUpdate(eventCode)
-	local isOnCooldown
-	local timeRemaining
+function GROUP_SYNERGIZER.OnCooldownsUpdate()
 	local cooldownType = {
 		a = LFG_COOLDOWN_ACTIVITY_STARTED, -- left dungeon group early (15 minute cooldown)
 		b = LFG_COOLDOWN_BATTLEGROUND_DESERTED, -- left battleground group early (5 minute cooldown)
@@ -139,20 +115,11 @@ function GROUP_SYNERGIZER.OnCooldownsUpdate(eventCode)
 		g = LFG_COOLDOWN_QUEUE_LEFT, -- declined ready check (30s cooldown)
 		h = LFG_COOLDOWN_TRIAL_LEFT,
 	}
-
 	for k, cooldown in pairs(cooldownType) do
-		timeRemaining = GetLFGCooldownTimeRemainingSeconds(cooldown)
-		isOnCooldown = timeRemaining > 0
+		local timeRemaining = GetLFGCooldownTimeRemainingSeconds(cooldown)
+		local isOnCooldown = timeRemaining > 0
 		if GROUP_SYNERGIZER.coolDownStatus[cooldown] ~= nil then
 			GROUP_SYNERGIZER.coolDownStatus[cooldown] = isOnCooldown
-			--df("%s %s", k, (isOnCooldown and "YES" or "NO"))
-			--GROUP_SYNERGIZER.AutoAcceptCheckbox(not isOnCooldown)
 		end
-		--if timeRemaining > 0 then df("%s ==> %s %s", k, tostring(timeRemaining), tostring(isOnCooldown)) end
 	end
 end
-
---d(os.time(os.date("!*t"))
---d('LFG Activity in Progress')
---SoundNotifyPlayer(SOUNDS.LOCKPICKING_BREAK)
---messageParams:SetSound(SOUNDS.DISPLAY_ANNOUNCEMENT)
